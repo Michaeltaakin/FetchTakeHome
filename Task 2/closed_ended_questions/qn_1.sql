e3d6c0e90d736155bbb4dfec573ccac921b8aaa1
Mike Akin <michaeltaakin@gmail.com>
2025-04-23 15:30:58 -0400

    adding aliases and order desc to task 2 - close ended questions

--- Task 2/closed_ended_questions/qn_1.sql
+++ Task 2/closed_ended_questions/qn_1.sql
@@ -1,9 +1,10 @@
 -- SQL Server Query
 -- What are the top 5 brands by receipts scanned among users 21 and over
 SELECT 
-    top 5 brand, count(distinct receipt_id)
+    top 5 brand, count(distinct receipt_id) total_receipts
 FROM transactions t 
 LEFT JOIN products p on t.barcode = p.barcode
 LEFT JOIN users u on t.user_id = u.id 
 WHERE Datediff(YEAR, u.birthdate, getdate()) >= 21
 GROUP BY brand
+ORDER BY count(distinct receipt_id) desc
--- Task 2/closed_ended_questions/qn_2.sql
+++ Task 2/closed_ended_questions/qn_2.sql
@@ -2,9 +2,10 @@
 -- What are the top 5 brands by sales amoung users that have had their account for at least six months
 
 SELECT 
-    top 5 brand, sum(final_sales)
+    top 5 brand, sum(final_sales) total_sales
 FROM transactions t 
 LEFT JOIN products p on t.barcode = p.barcode
 LEFT JOIN users u on t.user_id = u.id 
 WHERE Datediff(Month, u.created_date, (select max(u.created_date) from users)) >= 6
 GROUP BY brand
+ORDER BY sum(final_sales) desc
